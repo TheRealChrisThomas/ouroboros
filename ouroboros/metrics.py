9b690ca82a916eb12da3c71f8d2cbb348bc5bdc9
circa10a <caleblemoine@gmail.comS>
2018-10-27 12:47:13 -0500

    damn tests

--- ouroboros/metrics.py
+++ ouroboros/metrics.py
@@ -1,10 +1,11 @@
 from prometheus_client import Counter
 
+updated_containers_counter = Counter(
+    'containers_updated', 'Count of containers updated', ['container'])
+
 
 def initialize_container_update_counter():
     """Initialize updated_containers_counter when ouroboros starts"""
-    updated_containers_counter = Counter(
-        'containers_updated', 'Count of containers updated', ['container'])
     updated_containers_counter.labels(container='all')
     return updated_containers_counter
 
